3c89c026113650df6a22b5912eac6527fbdcd655
Akshay Chauhan <52008891+akshaychauhan-ac@users.noreply.github.com>
2020-08-16 14:26:17 +0530

    Update dfs.js

--- graphs/dfs.js
+++ graphs/dfs.js
@@ -1,10 +1,10 @@
 const visited = new Set();
-		const dfs = node => {
-			visit(node);
-			visited.add(node);
-			for (let neighbour of adjacencyList.get(node)) {
-				if (!visited.has(neighbour)) {
-					dfs(neighbour);
-				}
-			}
-		};
+const dfs = node => {
+	visit(node);
+	visited.add(node);
+	for (let neighbour of adjacencyList.get(node)) {
+		if (!visited.has(neighbour)) {
+			dfs(neighbour);
+		}
+	}
+};
