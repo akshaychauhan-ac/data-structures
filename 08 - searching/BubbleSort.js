c9a2389ae380503ba9d634507cba098d6362006e
Akshay <akshay.chauhan@sap.com>
2021-01-30 18:56:35 +0530

    Create BubbleSort.js

---
+++ 08 - searching/BubbleSort.js
@@ -0,0 +1,15 @@
+let bubbleSort = (aList) => {
+    let bSwapped = false;
+    do {
+        bSwapped = false;
+        for (let iItem = 0; iItem < aList.length; iItem++) {
+            if (aList[iItem] > aList[iItem + 1]) {
+                let iTemp = aList[iItem];
+                aList[iItem] = aList[iItem + 1];
+                aList[iItem + 1] = iTemp;
+                bSwapped = true;
+            }
+        }
+    } while (bSwapped);
+    return aList;
+};
