2391b81f0179306950d697553fb7c630c6d0edb7
Akshay Chauhan <52008891+akshaychauhan-ac@users.noreply.github.com>
2021-03-08 20:22:03 +0530

    Update QuickSort.js

--- 08 - sorting/QuickSort.js
+++ 08 - sorting/QuickSort.js
@@ -5,9 +5,9 @@ function swap(items, leftIndex, rightIndex){
 }
 
 function partition(items, left, right) {
-    var pivot   = items[Math.floor((right + left) / 2)], //middle element
-        i       = left, //left pointer
-        j       = right; //right pointer
+    var pivot = items[Math.floor((right + left) / 2)], //middle element
+        i = left, //left pointer
+        j = right; //right pointer
     while (i <= j) {
         while (items[i] < pivot) {
             i++;
