5517a3d7f01c7311fd5181629bcdf7030bac12ab
Akshay <akshay.chauhan@sap.com>
2021-06-24 18:28:16 +0530

    .

--- 08 - sorting/QuickSort.js
+++ 08 - sorting/QuickSort.js
@@ -1,13 +1,15 @@
 function swap(items, leftIndex, rightIndex){
-    var temp = items[leftIndex];
+    let temp = items[leftIndex];
+
     items[leftIndex] = items[rightIndex];
     items[rightIndex] = temp;
 }
 
 function partition(items, left, right) {
-    var pivot = items[Math.floor((right + left) / 2)], //middle element
+    let pivot = items[Math.floor((right + left) / 2)], //middle element
         i = left, //left pointer
         j = right; //right pointer
+
     while (i <= j) {
         while (items[i] < pivot) {
             i++;
@@ -25,8 +27,8 @@ function partition(items, left, right) {
 }
 
 function quickSort(items, left, right) {
-    debugger;
-    var index;
+    let index = 0;
+
     if (items.length > 1) {
         index = partition(items, left, right); //index returned from partition
         if (left < index - 1) { //more elements on the left side of the pivot
@@ -38,3 +40,6 @@ function quickSort(items, left, right) {
     }
     return items;
 }
+
+// let items = [9, 6, 4, 3, 8, 5, 4, 1, 2, 6];
+// let sortedArray = quickSort(items, 0, items.length - 1);
